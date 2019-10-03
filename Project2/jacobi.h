ca52ae1502285fcc6f79f5a61138a7014ec0eb22
kristoffer <kristowv@uio.no>
2019-10-03 11:45:56 +0200

    Cleaned up

--- Project2/jacobi.h
+++ Project2/jacobi.h
@@ -1,22 +1,12 @@
-#ifndef JACOBI_H
-#define	JACOBI_H
+#ifndef JACOBI_METHOD_H
+#define JACOBI_METHOD_H
 
 #include <iostream>
-#include<fstream>
-#include<math.h>
-#include<iomanip>
 #include <armadillo>
-#include<time.h>
-#include<vector>
 
-using namespace std;
+
 using namespace arma;
 
-void print_vals(mat,mat,int,double);
-void initialize(int,double,mat&,vec&,mat&,int,double);
-int jacobi(int,int,double,double,mat&,vec&,mat&);
-void find_max(mat,int&,int&,double&,int);
-vector<double> get_eigenvals(mat,int);
-mat get_eigenvecs(mat,mat,int);
+mat Jacobi(mat V, mat W, int k, int l, int n);
 
-#endif /* JACOBI_H */
+#endif // JACOBI_METHOD_H
