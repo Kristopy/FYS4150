5bb88cb6886a8e20affb2ac398e5957c0e4fe885
kristoffer <kristowv@uio.no>
2019-10-14 10:12:13 +0200

    Final

--- Project_3_main/main.cpp
+++ Project_3_main/main.cpp
@@ -3,8 +3,7 @@
 #include <fstream>
 #include <iomanip>  //setiosflags, setw, setprecision
 #include <string>
-//#include <stdio.h>
-//#include <stdlib.h>
+
 #include "repulsion_function.h"
 #include "integration_loops.h"
 
@@ -14,23 +13,8 @@ ofstream ofile;
 
 int main(int argc, char* argv[])
 {
-    string outfilename;
-    string method;
-    string both_algo;
-
-    // Declaration of command line arguments
-    if (argc <= 2){
-        cout << "Error occured: " << argv[0] <<endl;
-        cout <<  "Need do provide a commandline argument for which version to run, [Legendre, Laguerre, MonteCarlo], or make a file by passing inn [Project_3]" << endl;
-        exit(1);}
-    else{
-      outfilename = argv[1];            // First command line argument
-      method      = argv[2];            // Second command line argument
-      both_algo   = argv[3];            // Second command line argument
-    }
-
     // Assigning constant PI and calculating exact value of integral.
-    const double PI = std::atan(1.0)*4;
+    const double PI = atan(1.0)*4;
     double Exact_value = (5*pow(PI,2))/(256);
 
     //----------------------------------------------------------------------------------------------------------------------
@@ -52,7 +36,18 @@ int main(int argc, char* argv[])
     //----------------------------------------------------------------------------------------------------------------------
     //----------------------------------------------------------------------------------------------------------------------
 
-    if (argv[3]){
+    // Declaration of command line arguments
+    if (argc <= 1){
+        cout << "Error occured: " << argv[0] <<endl;
+        cout <<  "Need do provide a commandline argument for which version to run, [Legendre, Laguerre, MonteCarlo], or make a file by passing inn [Project_3]" << endl;
+        exit(1);
+    }
+    //----------------------------------------------------------------------------------------------------------------------
+    // If test for running both Legendre and laguere, and producing file.
+    //----------------------------------------------------------------------------------------------------------------------
+    else if (argc == 2){
+        string outfilename;
+        outfilename = argv[1];
         cout << "------------------------------------------------------------------------------ " << endl;
         cout << "Running Legedendre and Laguerre"<< endl;
         cout  << setiosflags(ios::showpoint | ios::uppercase);
@@ -90,29 +85,29 @@ int main(int argc, char* argv[])
 
         }
         //Loop ended
-        ofile.close();
-    }
-
+        ofile.close();}
     //----------------------------------------------------------------------------------------------------------------------
+    // If test for either Legendre or Laguerre
     //----------------------------------------------------------------------------------------------------------------------
+    else if (argc == 3){
+        string outfilename;
+        string method;
+        outfilename = argv[1];
+        method = argv[2];
+        if (method == "Legendre"){
+            double int_legendre, Legendre_time;
+            Gauss_legendre(a, b, n,int_legendre, Legendre_time);
 
-    if (argc < 2){
-
-        if (argc < 2 || method == "Legendre"){
-        double int_legendre, Legendre_time;
-        Gauss_legendre(a, b, n,int_legendre, Legendre_time);
-
-        cout << "------------------------------------------------------------------------------ " << endl;
-        cout << "Running Gaussian-Legendre quadrature"<< endl;
-        cout  << setiosflags(ios::showpoint | ios::uppercase);
-        cout << "------------------------------------------------------------------------------ " << endl;
-        cout << "Gaussian-Legendre quadrature = "<< setw(20) << setprecision(15)  << int_legendre << endl;
-        cout << "------------------------------------------------------------------------------ " << endl;
-        cout << "Excact result = "<< Exact_value << endl;
-        cout << "------------------------------------------------------------------------------ " << endl;
-
+            cout << "------------------------------------------------------------------------------ " << endl;
+            cout << "Running Gaussian-Legendre quadrature"<< endl;
+            cout  << setiosflags(ios::showpoint | ios::uppercase);
+            cout << "------------------------------------------------------------------------------ " << endl;
+            cout << "Gaussian-Legendre quadrature = "<< setw(20) << setprecision(15)  << int_legendre << endl;
+            cout << "------------------------------------------------------------------------------ " << endl;
+            cout << "Excact result = "<< Exact_value << endl;
+            cout << "------------------------------------------------------------------------------ " << endl;
         }
-        else if(argc < 2 || method == "Laguerre"){
+        else if (method == "Laguerre"){
             double int_laguerre,Laguerre_time;
             Gauss_laguerre(n,PI, int_laguerre, Laguerre_time);
 
