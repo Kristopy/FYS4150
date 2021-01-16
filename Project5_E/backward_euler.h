b867d5bb42c0e3a3cb7051eec1ae472b05175a3d
kristoffer <kristowv@uio.no>
2021-01-16 20:26:45 +0100

    Changes

--- Project5_E/backward_euler.h
+++
@@ -1,6 +0,0 @@
-#ifndef BACKWARD_EULER_H
-#define BACKWARD_EULER_H
-#include <armadillo>
-using namespace arma;
-void diffusion_BE(mat &u, int N, int n, vec v, rowvec w, rowvec z, bool save = 0);
-#endif // BACKWARD_EULER_H
